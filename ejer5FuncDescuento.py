bd49ac446489af0252381673d23ab5bb53ccec49
Alberto Llera Balbuena <mellowparasite@gmail.com>
2025-12-10 12:11:42 +0100

    Delete ejer5FuncDescuento.py

--- ejer5FuncDescuento.py
+++
@@ -1,6 +0,0 @@
-def calcular_precio(precio_base, descuento = 10):
-    return precio_base - ((precio_base * descuento) / 100)
-
-precio_producto = float(input("Por favor, introduzca el valor del producto: "))
-precio_pagar = calcular_precio(precio_producto)
-print(f"El precio final de producto es de: {round(precio_pagar, 2)}")
