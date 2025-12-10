ea86ec6049c7cb56c37afae2d9c09e66504d8d8c
AlbertoLlera <mellowparasite@gmail.com>
2025-12-10 12:29:23 +0100

    Primer commit repositorio python

--- ejer2FuncIVA.py
+++
@@ -1,6 +0,0 @@
-def calcular_precio(precio_base, iva = 21):
-    return precio_base * (1 + iva / 100)
-
-precio_producto = float(input("Por favor, introduzca el valor del producto: "))
-precio_pagar = calcular_precio(precio_producto)
-print(f"El precio final de producto es de: {round(precio_pagar, 2)}")
